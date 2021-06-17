7d0a3776ae299aa2d7677de486ecd77ad8005dda
Alexander Keijzer <alexanderkeijzer@gmail.com>
2021-06-17 12:10:09 +0200

    Remove dependency on scenic

--- eager_bridge_webots/src/eager_bridge_webots/webots_engine.py
+++ eager_bridge_webots/src/eager_bridge_webots/webots_engine.py
@@ -1,10 +1,6 @@
+from eager_bridge_webots.webots_parser import WebotsParser
 from eager_core.engine_params import EngineParams
 from eager_core.utils.file_utils import substitute_xml_args
-
-try:
-    from scenic.simulators.webots import world_parser
-except:
-    pass
 
 
 class WebotsEngine(EngineParams):
@@ -27,14 +23,16 @@ class WebotsEngine(EngineParams):
         # Calculate other parameters based on previously defined attributes.
         self.step_time = int(self.dt * 1000)
 
-        try:
-            # todo: check if problem that world_parser import requires python version > 3.7
-            wf = world_parser.parse(substitute_xml_args('%s' % world))  # Grab basicTimeStep from world file (.wbt).
-            val = world_parser.findNodeTypesIn(['WorldInfo'], wf, nodeClasses={})
-            self.basicTimeStep = int(val[0][0].attrs['basicTimeStep'][0])
+        wp = WebotsParser()
+        wp.load(substitute_xml_args('%s' % world))
+        for f in wp.content['root']:
+            for field in f['fields']:
+                if field['name'] == 'basicTimeStep':
+                    self.basicTimeStep = int(field['value'])
+                    break
+            else:
+                continue
+            break
 
-            # Error check the parameters here.
-            if self.step_time % self.basicTimeStep != 0:
-                raise RuntimeError('The steptime (%d ms) is not a multiple of the basicTimeStep (%d ms).' % (self.step_time, self.basicTimeStep))
-        except:
-            pass
+        if self.step_time % self.basicTimeStep != 0:
+            raise RuntimeError('The steptime (%d ms) is not a multiple of the basicTimeStep (%d ms).' % (self.step_time, self.basicTimeStep))
