7d3c06fdeb33dfc42eb1514bdffef3e1947377f0
Alexander Keijzer <alexanderkeijzer@gmail.com>
2021-06-17 11:14:06 +0200

    Revert "Temporarily remove try block"
    This reverts commit 17a3dad229de5e56888caccad81212f340bf1590.

--- eager_core/src/eager_core/utils/file_utils.py
+++ eager_core/src/eager_core/utils/file_utils.py
@@ -4,9 +4,12 @@ from six import raise_from
 
 
 def load_yaml(package_name, object_name):
-    pp = rospkg.RosPack().get_path(package_name)
-    filename = pp + "/config/" + object_name + ".yaml"
-    params = rosparam.load_file(filename)[0][0]
+    try:
+        pp = rospkg.RosPack().get_path(package_name)
+        filename = pp + "/config/" + object_name + ".yaml"
+        params = rosparam.load_file(filename)[0][0]
+    except Exception as ex:
+        raise_from(RuntimeError(('Unable to load %s from package %s' % (object_name, package_name))), ex)
     return params
 
 
