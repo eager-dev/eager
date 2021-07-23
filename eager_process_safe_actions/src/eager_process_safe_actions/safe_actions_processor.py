4fd60b2434211ee6f8ab2591972f0120e8b335df
jelledouwe <j.d.luijkx@tudelft.nl>
2021-07-23 10:51:33 +0200

    Allow user to specify height of collisions

--- eager_process_safe_actions/src/eager_process_safe_actions/safe_actions_processor.py
+++ eager_process_safe_actions/src/eager_process_safe_actions/safe_actions_processor.py
@@ -11,7 +11,10 @@ class SafeActionsProcessor(EngineParams):
                  object_frame: str,
                  checks_per_rad: int,
                  duration: float,
-                 vel_limit: float):
+                 vel_limit: float,
+                 collision_height: float,
+                 base_length: float,
+                 workspace_length: float):
         kwargs = locals().copy()
         kwargs.pop('self')
         self.launch_args = kwargs
